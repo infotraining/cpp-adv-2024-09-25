532d88012f113b1e302018d83bdb4633031bd55f
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-09-25 12:11:18 +0200

    UniquePtr - move semantics

--- move-semantics/move_semantics_1.cpp
+++ move-semantics/move_semantics_1.cpp
@@ -16,11 +16,44 @@ TEST_CASE("reference binding")
 
     SECTION("C++98")
     {
-        // TODO
+        std::string& lvref_name = name;
+        const std::string& ref_full_name = full_name(name, "Kowalski");
+        // ref_full_name[0] = 'J';
     }
 
     SECTION("C++11")
     {
-        // TODO
+        std::string&& rvref_full_name = full_name(name, "Kowalski");
+        rvref_full_name[0] = 'J';
+        REQUIRE(rvref_full_name == "Jan Kowalski");
+
+        // std::string&& rvref_name = name; // ERROR - You cannot bind an lvalue to an rvalue reference
     }
+}
+
+class Vector
+{
+public:
+    void push_back(const std::string& s)
+    {
+        std::cout << "push_back(const string&) - makes copy of " << s << " to vector\n";
+    }
+
+    void push_back(std::string&& s)
+    {
+        std::cout << "push_back(string&&) - moves " << s << " to vector\n";
+    }
+};
+
+TEST_CASE("function overloading with references")
+{
+    std::string str = "text";
+
+    Vector vec;
+
+    vec.push_back(str);
+    vec.push_back(str + str);
+    vec.push_back("ctext");
+    vec.push_back(static_cast<std::string&&>(str));
+    vec.push_back(str);
 }
--- move-semantics/move_semantics_2.cpp
+++ move-semantics/move_semantics_2.cpp
@@ -1,6 +1,7 @@
 #include "gadget.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+#include <vector>
 
 ////////////////////////////////////////////////
 // simplified implementation of unique_ptr - only moveable type
@@ -8,12 +9,109 @@
 namespace Explain
 {
     template <typename T>
-    class unique_ptr
+    class UniquePtr
     {
-        // TODO
+        T* object_ptr;
+
+    public:
+        explicit UniquePtr(T* object)
+            : object_ptr{object}
+        {
+        }
+
+        UniquePtr(const UniquePtr& other) = delete;
+        UniquePtr& operator=(const UniquePtr& other) = delete;
+
+        // move constructor
+        UniquePtr(UniquePtr&& other)
+            : object_ptr{other.object_ptr}
+        {
+            other.object_ptr = nullptr;
+        }
+
+        // move assignment operator
+        UniquePtr& operator=(UniquePtr&& other)
+        {
+            if (this != &other)
+            {
+                delete object_ptr; // release of previous state
+
+                object_ptr = other.object_ptr;
+                other.object_ptr = nullptr;
+            }
+
+            return *this;
+        }
+
+        ~UniquePtr()
+        {
+            delete object_ptr;
+        }
+
+        explicit operator bool() const
+        {
+            return object_ptr != nullptr;
+        }
+
+        T& operator*() const
+        {
+            return *object_ptr;
+        }
+
+        T* operator->() const
+        {
+            return object_ptr;
+        }
+
+        T* get() const
+        {
+            return object_ptr;
+        }
     };
 } // namespace Explain
 
+Explain::UniquePtr<Helpers::Gadget> create_gadget()
+{
+    using Helpers::Gadget;
+    static int id_gen = 0;
+    const int id = ++id_gen;
+    return Explain::UniquePtr<Gadget>{new Gadget{id, "Gadget#" + std::to_string(id)}};
+}
+
 TEST_CASE("move semantics - unique_ptr")
 {
+    using Explain::UniquePtr;
+    using Helpers::Gadget;
+
+    UniquePtr<Gadget> ptr1{new Gadget(1, "ipad")};
+    if (ptr1)
+    {
+        (*ptr1).use();
+        ptr1->use();
+    }
+
+    UniquePtr<Gadget> ptr2 = std::move(ptr1); // call of move constructor
+    if (ptr2)
+        ptr2->use();
+    assert(ptr1.get() == nullptr);
+
+    UniquePtr<Gadget> ptr3{new Gadget{42, "smartwatch"}};
+    ptr3->use();
+    ptr3 = std::move(ptr2); // call of move assignment operator
+    ptr3->use();
+
+    ptr3 = std::move(ptr3); // self assignment
+
+    std::vector<UniquePtr<Gadget>> gadgets;
+    gadgets.push_back(std::move(ptr3));
+    gadgets.push_back(create_gadget());
+    gadgets.push_back(create_gadget());
+    gadgets.push_back(create_gadget());
+    gadgets.push_back(create_gadget());
+
+    for(const auto& g : gadgets)
+    {
+        if (g)
+            g->use();
+    }
 }
