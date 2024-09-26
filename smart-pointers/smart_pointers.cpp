6e7d5d4172a9f5343cfdba927041988cca8634b7
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-09-26 12:19:02 +0200

    unique_ptrs

--- smart-pointers/smart_pointers.cpp
+++ smart-pointers/smart_pointers.cpp
@@ -35,6 +35,11 @@ namespace LegacyCode
         if (g)
             std::cout << "Using " << g->name() << "\n";
     }
+
+    void free_gadget(Gadget* g)
+    {
+        delete g;
+    }
 } // namespace LegacyCode
 
 TEST_CASE("legacy hell with dynamic memory")
@@ -53,7 +58,6 @@ TEST_CASE("legacy hell with dynamic memory")
 
     {
         Gadget* g = new Gadget(13, "ipad");
-        
 
         use(g);
         // use_gadget(g);  // UB!!! - use after delete
@@ -62,10 +66,109 @@ TEST_CASE("legacy hell with dynamic memory")
 
     {
         Gadget* g = get_gadget("ipad");
-        
 
         use(g);
 
-        // delete g; // UB!!! - second delete
+        delete g; // UB!!! - second delete
     }
 }
+
+namespace ModernCpp
+{
+    std::unique_ptr<Gadget> get_gadget(const std::string& name);
+    void use(std::unique_ptr<Gadget> g);
+
+    std::unique_ptr<Gadget> get_gadget(const std::string& name)
+    {
+        static int id = 0;
+        return std::make_unique<Gadget>(++id, name); // RVO
+    }
+
+    void use(std::unique_ptr<Gadget> g)
+    {
+        if (g)
+            std::cout << "Using " << g->name() << "\n";
+    }
+
+    void use_gadget(Gadget* g)
+    {
+        if (g)
+            std::cout << "Using " << g->name() << "\n";
+    }
+
+} // namespace ModernCpp
+
+TEST_CASE("modern c++ with smart pointers")
+{
+    using namespace ModernCpp;
+
+    {
+        std::unique_ptr<Gadget> g = get_gadget("ipad");
+
+        use_gadget(g.get());
+    }
+
+    {
+        use(get_gadget("ipad"));
+    }
+
+    {
+        auto g = std::make_unique<Gadget>(13, "ipad");
+
+        use(std::move(g));
+    }
+
+    {
+        std::unique_ptr<Gadget> g = get_gadget("ipad");
+
+        use(std::move(g));
+    }
+
+    {
+        std::unique_ptr<Gadget> g = get_gadget("ipad");
+
+        LegacyCode::free_gadget(g.release());
+    }
+}
+
+void may_throw()
+{
+    throw std::runtime_error("ERROR#14");
+}
+
+TEST_CASE("custom deallocators")
+{
+    SECTION("legacy")
+    {
+        FILE* fin = fopen("text.txt", "w+");
+
+        fprintf(fin, "test");
+
+        //may_throw();
+
+        fclose(fin);      
+    }
+
+    SECTION("modern")
+    {
+        std::unique_ptr<FILE, int(*)(FILE*)> fin{fopen("text.txt", "w+"), fclose};
+
+        fprintf(fin.get(), "test");
+
+        //may_throw();
+    }
+}
+
+namespace LegacyCode
+{
+    int* create_buffer()
+    {
+        return new int[1024];
+    }
+}
+
+TEST_CASE("T[] specialization")
+{
+    std::unique_ptr<int[]> buffer{LegacyCode::create_buffer()};
+    buffer[512] = 42;
+} // delete[]
